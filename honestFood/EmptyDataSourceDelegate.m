dc2290e992626fc3dfaba4701f2ce7c459974507
Tuan Vu <s2.4home@gmail.com>
2016-12-17 14:51:34 +0700

    add emptydataset to scroll view-

---
+++ honestFood/EmptyDataSourceDelegate.m
@@ -0,0 +1,72 @@
+//
+//  EmptyDataSourceDelegate.m
+//  honestFood
+//
+//  Created by Tuan Vu on 12/17/16.
+//  Copyright © 2016 tuanvu. All rights reserved.
+//
+
+#import "EmptyDataSourceDelegate.h"
+
+@implementation EmptyDataSourceDelegate
+
+
++ (instancetype)sharedInstance
+{
+    static EmptyDataSourceDelegate *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[EmptyDataSourceDelegate alloc] init];
+        // Do any other initialisation stuff here
+    });
+    return sharedInstance;
+}
+
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView
+{
+    return [self image:[UIImage imageNamed:@"food-holder"] scaledToSize:CGSizeMake(80, 80)];
+}
+
+- (UIImage *)image:(UIImage*)originalImage scaledToSize:(CGSize)size
+{
+    //avoid redundant drawing
+    if (CGSizeEqualToSize(originalImage.size, size))
+    {
+        return originalImage;
+    }
+    
+    //create drawing context
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
+    
+    //draw
+    [originalImage drawInRect:CGRectMake(0.0f, 0.0f, size.width, size.height)];
+    
+    //capture resultant image
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    //return image
+    return image;
+}
+- (NSAttributedString *)descriptionForEmptyDataSet:(UIScrollView *)scrollView
+{
+    NSString *text = @"chọn món ngay nào!";
+    
+    NSMutableParagraphStyle *paragraph = [NSMutableParagraphStyle new];
+    paragraph.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraph.alignment = NSTextAlignmentCenter;
+    
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:15.0f],
+                                 NSForegroundColorAttributeName: [UIColor lightGrayColor],
+                                 NSParagraphStyleAttributeName: paragraph};
+    
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
+}
+
+//- (UIImage *)buttonImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state
+//{
+//    return [self image:[UIImage imageNamed:@"btnEmptyDataSet.png"] scaledToSize:CGSizeMake(195, 50)];
+//}
+
+
+@end
